bfa4f29704fd8456e0c7c7fcfbd0e338910e0769
atmetz <atmetz@gmail.com>
2025-12-07 17:24:07 -0600

    added pause

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"os/signal"
+	"log"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -19,9 +20,17 @@ func main() {
 
 	fmt.Println("Connection was successful")
 
-	signalChan := make(chan os.Signal, 1)
+	newCh, err := connection.Channel()
+	if err != nil {
+		log.Fatalf("could not create channel: %v", err)
+	}
 
-	signal.Notify(signalChan, os.Interrupt)
-	<-signalChan
-	fmt.Println("exiting")
+	err = pubsub.PublishJSON(newCh, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{
+		IsPaused: true,
+	})
+	if err != nil {
+		log.Printf("could not publish time: %v", err)
+	}
+
+	fmt.Println("Pause message sent!")
 }
